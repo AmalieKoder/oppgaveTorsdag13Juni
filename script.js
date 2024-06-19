c6ecb9e29137f4d504b583adcb05c8dc04e6bd33
AmalieKoder <amlie.koder@gmail.com>
2024-06-19 10:20:43 +0200

    index

--- script.js
+++ script.js
@@ -1,29 +1,68 @@
-/*// Constructor function
-function character(firstName, lastName,furrColor, eyes, traits) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.furrColor = furrColor;
-    this.eyes = eyes;
-    this.traits = traits;
+//Get the id from the HTML and place it in JavaScript
+const randomCatInfo = document.querySelector("#random");
+const infoCharacter = document.querySelector("#infoCharacter");
+const traitsInfo = document.querySelector("#infoLink")
+const randomNameBtn = document.querySelector("#randomName");
+const textName = document.querySelector("#textName");
+
+// Array of possible fur colors
+let furrColor = ["Red" , "Gray" , "Black" , "White" , "Cream", "Blue", "Cinnamon", "Fawn", "Brown"];
+
+// Array of possible cat breeds
+let breeds = ["Sjameser", "Maine coon", "Sphynx", "Hellig birma", "Munchkin", "Ragdoll", "Norsk skogkatt"]
+
+// Array of possible traits
+let trait = ["Aloof",  "Fluffy",  "Free Spirit",  "Frendly",   "Lazy",  "Talkative",  "PlayFull", "Curious", "Prowler"];
+
+// Array of possible eye colors
+let eyesColor = ["Blue", "Hazel", "Yellow", "Amber", "Green", "Orange", "Copper"]
+
+
+// Random generater for furrColor, trait, eyesColor and breeds
+function random(furrColor) {
+    return furrColor[Math.floor(Math.random()*furrColor.length)];
 }
 
-// Creating an instance of the Character
-const amalieCatsCharacter = new character("Yuki", "Snow", "whiteCream", "blue", "");
-
-
-
-
-// Function to display the character
-function displayCharacter(character) {
-    const myCharacter = document.querySelector("#myCharacter");
-    const infoCharacter = document.querySelector("#infoCharacter");
-   
-    myCharacter.textContent = 
-    `${character.firstName} ${character.lastName}`;
-
-    infoCharacter.textContent =  `Yuki furr color is ${character.furrColor} and her eyes are ${character.eyes}.
-    Shes have a fluffy tail, and her trait is `;
+function random(trait) {
+    return trait[Math.floor(Math.random()*trait.length)];
 }
 
-// Wait for the DOM to be fully loaded before running the script
- displayCharacter(amalieCatsCharacter);*/
+function random(eyesColor) {
+    return eyesColor[Math.floor(Math.random()*eyesColor.length)];
+}
+
+function random(breeds) {
+    return breeds[Math.floor(Math.random()*breeds.length)];
+}
+
+// Event listener for the random catsInfo button
+randomCatInfo.addEventListener("click", () => {
+    infoCharacter.textContent = `furColor: ${random(furrColor)} and ${random(furrColor)}, traits: ${random(trait)}, eyesColor: ${random(eyesColor)}, breed: ${random(breeds)}`
+
+});
+
+// Arrays of possible FirstName And LastName
+let firstName = ["Yuki", "Kuro", "Cleo", "Petunia", "Maria", "Muffin", "Leo", "Mira", "Finn", "Fant", "Shadow"]
+let lastName = ["Snow", "Miller", "Marshall", "Kim", "Mork", "Volden", "Hamlingthon", "Jr", "Valantine", "Merlands"]
+
+//Random generater for FirstName and LastName
+function random(lastName) {
+    return lastName[Math.floor(Math.random()*lastName.length)];
+}
+
+function random(firstName) {
+    return firstName[Math.floor(Math.random()*firstName.length)];
+}
+
+//Event listener for the random names button
+randomNameBtn.addEventListener("click", () => {
+    textName.value = `${random(firstName)} ${random(lastName)}`;
+});
+
+
+
+
+
+
+
+
